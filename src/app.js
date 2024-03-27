0d53b66e7a4d418fe5cac6e4c21f20ba51957ba7
mdwulandari <nimadewulandari9@gmail.com>
2024-03-28 07:34:11 +0800

    new edit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import Express from "express";
 import Cors from "cors";
-
 import Config from "./config.js";
+
+import mysql from "mysql";
 
 const app = Express();
 
@@ -36,4 +37,85 @@ app.get("/routing", (req, res) => {
 
 app.listen(Config.PORT, () => {
   console.log(`Example app listening on port ${Config.PORT}`);
+});
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "db_2105551075",
+  port: 3306,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database:", err);
+    return;
+  }
+  console.log("Connected to MySQL database");
+});
+
+app.post("/process", (req, res) => {
+  const { lat_lng, nama_rs, alamat_rs, type_rs} = req.body;
+
+  if (!lat_lng || !nama_rs || !alamat_rs || !type_rs) {
+    res.status(400).send('Data tidak lengkap');
+    console.log('Data yang diterima:', req.body);
+    return;
+  }
+
+  const sql = `INSERT INTO tb_rs (lat_lng, nama_rs, alamat_rs, type_rs) VALUES (?, ?, ?, ?)`;
+  const values = [lat_lng, nama_rs, alamat_rs, type_rs];
+
+  connection.query(sql, values, (err, result) => {
+    if (err) {
+      console.error("Error executing SQL query:", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+    console.log("Data inserted into database");
+    console.log('Data yang diterima:', req.body);
+    res.send("Data inserted into database");
+  });
+});
+
+var btnKirim = document.getElementById('btnKirim');
+        btnKirim.addEventListener('click', function() {
+            var namaRS = document.getElementById('namaRS').value;
+            var latitude = document.getElementById('latitude').value;
+            var longitude = document.getElementById('longitude').value;
+            var alamatRS = document.getElementById('alamatRS').value;
+
+            var data = {
+                nama_rs: namaRS,
+                latlng_rs: latitude + ',' + longitude,
+                alamat_rs: alamatRS
+            };
+
+            fetch("/process", { 
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then(function(response) {
+                return response.json();
+            }).then(function(data) {
+                console.log(data);
+            }).catch(function(error) {
+                console.log(error);
+            });
+        });
+
+        fetch("/getData").then(function(response) { 
+            return response.json();
+        }).then(function(data) {
+            data.forEach(function(c, i) {
+                var latlng = L.latLng(c.latlng_rs.split(',')[0], c.latlng_rs.split(',')[1]); 
+                var randomHospitalName = hospitalNames[Math.floor(Math.random() * hospitalNames.length)];
+                var newMarker = addMarker(latlng, markers.length, randomHospitalName);
+                markers.push(newMarker);
+            })
+        }).catch(function(error) {
+            console.log(error);
 });
