a66227a6b2b2b28ba665e993a96a2a98d65ffaf2
mdwulandari <nimadewulandari9@gmail.com>
2024-05-13 19:06:47 +0800

    models

--- app/Models/Hospital.php
+++ app/Models/Hospital.php
@@ -9,39 +9,6 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class Hospital extends Authenticatable implements JWTSubject
 {
-    use Notifiable;
-
-    public $timestamps = false;
-
-    protected $table = 'tb_rs';
     
-    protected $primaryKey = 'id_rs';
-
-    protected $fillable = [
-        'lat_lng_rs',
-        'nama_rs',
-        'alamat_rs',
-        'id_type_rs',
-    ];
-
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
-
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
 }
 
